b24b32d3a0c5c38b622b6e774ad1b562cd67c06c
valehelle <hazmi.irfan.zainal@rhbgroup.com>
2018-11-09 11:05:18 +0800

    update router

--- lib/sapphire_web/router.ex
+++ lib/sapphire_web/router.ex
@@ -22,13 +22,23 @@ defmodule SapphireWeb.Router do
   end
 
   scope "/", SapphireWeb do
-  pipe_through [:browser, :auth] # Use the default browser stack
-  get "/", PageController, :index
-  get "/users/sign_up", UserController, :register
-  post "/users/sign_up", UserController, :register_user
-  get "/users/sign_in", UserController, :index
-  post "/users/sign_in", UserController, :login
-  post "/users/sign_out", UserController, :logout
+    pipe_through [:browser, :auth] # Use the default browser stack
+    get "/", PageController, :index
+    get "/users/sign_up", UserController, :register
+    post "/users/sign_up", UserController, :register_user
+    get "/users/sign_in", UserController, :index
+    post "/users/sign_in", UserController, :login
+    post "/users/sign_out", UserController, :logout
+
+  end
+
+  scope "/", SapphireWeb do
+    pipe_through [:api] # Use the default browser stack
+
+    get "/project/:project_id/api/*routes", EndpointController, :get_api
+    post "/project/:project_id/api/*routes", EndpointController, :get_api
+    put "/project/:project_id/api/*routes", EndpointController, :get_api
+    delete "/project/:project_id/api/*routes", EndpointController, :get_api
   end
 
   scope "/", SapphireWeb do
@@ -46,11 +56,6 @@ defmodule SapphireWeb.Router do
     get "/project/:project_id/endpoint/:endpoint_id", EndpointController, :show
     get "/project/:project_id/endpoint/:endpoint_id/edit", EndpointController, :edit
     put "/project/:project_id/endpoint/:endpoint_id/edit", EndpointController, :update
-
-    get "/project/:project_id/api/*routes", EndpointController, :get_api
-    post "/project/:project_id/api/*routes", EndpointController, :get_api
-    put "/project/:project_id/api/*routes", EndpointController, :get_api
-    delete "/project/:project_id/api/*routes", EndpointController, :get_api
   end
   # Other scopes may use custom stacks.
   # scope "/api", SapphireWeb do
