f267e7b62ef5309b2622fec922bbcf9aba18ea90
valehelle <hazmi.irfan.zainal@rhbgroup.com>
2018-11-09 15:41:55 +0800

    fix path

--- lib/sapphire_web/views/endpoint_view.ex
+++ lib/sapphire_web/views/endpoint_view.ex
@@ -3,7 +3,8 @@ defmodule SapphireWeb.EndpointView do
 
   def getAPIPath(conn, project_id) do
     cur_uri = SapphireWeb.Endpoint.struct_url()
+    base_url = %URI{cur_uri | scheme: "https" , port: nil}
     api_path = endpoint_path(conn, :get_api, project_id, [])
-    path = SapphireWeb.Router.Helpers.url(%URI{cur_uri | scheme: "https"}) <> api_path
+    path = SapphireWeb.Router.Helpers.url(base_url) <> api_path
   end
 end
